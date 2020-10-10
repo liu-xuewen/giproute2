f00de9536485ca9e21c56d932909fbd0fa6a13f6
liuxuewen <liiuxuewen@gmail.com>
2020-10-10 18:05:23 +0800

    feat: add gomodule

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/liu-xuewen/giproute2
+
+go 1.13
+
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	github.com/vishvananda/netlink v1.1.0
+)
--- ip.go
+++ ip.go
@@ -7,14 +7,15 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "github.com/docopt/docopt-go"
-    "github.com/vishvananda/netlink"
+	"fmt"
+	"net"
+
+	"github.com/docopt/docopt-go"
+	"github.com/vishvananda/netlink"
 )
 
 func main() {
-    usage := `giproute2 is a iproute2 rewritten in Go.
+	usage := `giproute2 is a iproute2 rewritten in Go.
 
 Usage:
   ip link show
@@ -23,122 +24,122 @@ Usage:
   ip route (show | list)
   ip route add <dst> dev <ifname> [via <gateway>] [src <src>]`
 
-    args, _ := docopt.Parse(usage, nil, true, "giproute2 0.0.1", false)
-    if args["link"] == true {
-        if args["show"] == true {
-            linkShow()
-        } else if args["set"] == true {
-            if args["up"] == true {
-                linkSetUp(args["<ifname>"].(string))
-            } else if args["down"] == true {
-                linkSetDown(args["<ifname>"].(string))
-            }
-        }
-    } else if args["address"] == true {
-        if args["add"] == true {
-            addrAdd(args["<ifaddr>"].(string), args["<ifname>"].(string))
-        }
-    } else if args["route"] == true {
-        if args["add"] == true {
-            dst := ""
-            if args["<dst>"] != nil {
-                dst = args["<dst>"].(string)
-            }
+	args, _ := docopt.Parse(usage, nil, true, "giproute2 0.0.1", false)
+	if args["link"] == true {
+		if args["show"] == true {
+			linkShow()
+		} else if args["set"] == true {
+			if args["up"] == true {
+				linkSetUp(args["<ifname>"].(string))
+			} else if args["down"] == true {
+				linkSetDown(args["<ifname>"].(string))
+			}
+		}
+	} else if args["address"] == true {
+		if args["add"] == true {
+			addrAdd(args["<ifaddr>"].(string), args["<ifname>"].(string))
+		}
+	} else if args["route"] == true {
+		if args["add"] == true {
+			dst := ""
+			if args["<dst>"] != nil {
+				dst = args["<dst>"].(string)
+			}
 
-            gateway := ""
-            if args["<gateway>"] != nil {
-                gateway = args["<gateway>"].(string)
-            }
-            ifname := ""
-            if args["<ifname>"] != nil {
-                ifname = args["<ifname>"].(string)
-            }
+			gateway := ""
+			if args["<gateway>"] != nil {
+				gateway = args["<gateway>"].(string)
+			}
+			ifname := ""
+			if args["<ifname>"] != nil {
+				ifname = args["<ifname>"].(string)
+			}
 
-            src := ""
-            if args["<src>"] != nil {
-                src = args["<src>"].(string)
-            }
-            routeAdd(dst, gateway, ifname, src)
-        } else {
-            routeShow()
-        }
-    }
+			src := ""
+			if args["<src>"] != nil {
+				src = args["<src>"].(string)
+			}
+			routeAdd(dst, gateway, ifname, src)
+		} else {
+			routeShow()
+		}
+	}
 }
 
 func linkShow() {
-    links, err := netlink.LinkList()
-    if err != nil {
-        fmt.Println(err)
-    }
-    for _, link := range links {
-        fmt.Println(link.Attrs())
-    }
+	links, err := netlink.LinkList()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, link := range links {
+		fmt.Println(link.Attrs())
+	}
 }
 
 func addrAdd(ifaddr string, ifname string) {
-    link, err := netlink.LinkByName(ifname)
-    if err != nil {
-        fmt.Println(err)
-    }
-    addr, err := netlink.ParseAddr(ifaddr)
-    if err != nil {
-        fmt.Println(err)
-    }
-    err = netlink.AddrAdd(link, addr)
-    if err != nil {
-        fmt.Println(err)
-    }
+	link, err := netlink.LinkByName(ifname)
+	if err != nil {
+		fmt.Println(err)
+	}
+	addr, err := netlink.ParseAddr(ifaddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = netlink.AddrAdd(link, addr)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func routeShow() {
-    links, err := netlink.LinkList()
-    if err != nil {
-        fmt.Println(err)
-    }
-    for _, link := range links {
-        routes, _ := netlink.RouteList(link, netlink.FAMILY_V4)
-        for _, route := range routes {
-            fmt.Println(route)
-        }
-    }
+	links, err := netlink.LinkList()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, link := range links {
+		routes, _ := netlink.RouteList(link, netlink.FAMILY_V4)
+		for _, route := range routes {
+			fmt.Println(route)
+		}
+	}
 }
 
 func routeAdd(dst string, gateway string, ifname string, src string) {
-    link, err := netlink.LinkByName(ifname)
-    if err != nil {
-        fmt.Println(err)
-    }
-    route := netlink.Route{LinkIndex: link.Attrs().Index}
-    if dst != "default" {
-        dstAddr, _ := netlink.ParseAddr(dst)
-        route.Dst = dstAddr.IPNet
-    }
-    if src != "" {
-        srcIP := net.ParseIP(src)
-        route.Src = srcIP
-    }
-    if gateway != "" {
-        gatewayIP := net.ParseIP(gateway)
-        route.Gw = gatewayIP
-    }
-    err = netlink.RouteAdd(&route)
-    if err != nil {
-        fmt.Println(err)
-    }
+	link, err := netlink.LinkByName(ifname)
+	if err != nil {
+		fmt.Println(err)
+	}
+	route := netlink.Route{LinkIndex: link.Attrs().Index}
+	if dst != "default" {
+		dstAddr, _ := netlink.ParseAddr(dst)
+		route.Dst = dstAddr.IPNet
+	}
+	if src != "" {
+		srcIP := net.ParseIP(src)
+		route.Src = srcIP
+	}
+	if gateway != "" {
+		gatewayIP := net.ParseIP(gateway)
+		route.Gw = gatewayIP
+	}
+	err = netlink.RouteAdd(&route)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func linkSetUp(ifname string) {
-    link, err := netlink.LinkByName(ifname)
-    netlink.LinkSetUp(link)
-    if err != nil {
-        fmt.Println(err)
-    }
+	link, err := netlink.LinkByName(ifname)
+	netlink.LinkSetUp(link)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func linkSetDown(ifname string) {
-    link, err := netlink.LinkByName(ifname)
-    netlink.LinkSetDown(link)
-    if err != nil {
-        fmt.Println(err)
-    }
+	link, err := netlink.LinkByName(ifname)
+	netlink.LinkSetDown(link)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
